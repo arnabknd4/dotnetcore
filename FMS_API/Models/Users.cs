41cdc8ca2e6272c0374a06ca443e73137a89e1ca
Arnab Kundu <Arnab.Kundu6@cognizant.com>
2019-02-26 10:12:51 -0800

    Initial Data model added

---
+++ FMS_API/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FMS_API.Models
+{
+    public partial class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int? RoleId { get; set; }
+
+        public Roles Role { get; set; }
+    }
+}
